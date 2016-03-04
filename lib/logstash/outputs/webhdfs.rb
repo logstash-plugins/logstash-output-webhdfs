1fa418bf8b49b82a338bd4a99f5ca19087ca5a68
dstore-dbap <b.puttmann@dbap.de>
2016-03-04 15:49:25 +0100

    Added support for kerberos and ssl authentication. New config parameters introduced: use_kerberos_auth kerberos_keytab use_ssl_auth ssl_key

--- lib/logstash/outputs/webhdfs.rb
+++ lib/logstash/outputs/webhdfs.rb
@@ -105,6 +105,21 @@ class LogStash::Outputs::WebHdfs < LogStash::Outputs::Base
   # Set snappy format. One of "stream", "file". Set to stream to be hive compatible.
   config :snappy_format, :validate => ["stream", "file"], :default => "stream"
 
+  # Set kerberos authentication.
+  config :use_kerberos_auth, :validate => :boolean, :default => false
+
+  # Set kerberos keytab file. Note that the gssapi library needs to be available to use this.
+  config :kerberos_keytab, :validate => :string
+
+  # Set ssl authentication. Note that the openssl library needs to be available to use this.
+  config :use_ssl_auth, :validate => :boolean, :default => false
+
+  # Set ssl key file.
+  config :ssl_key, :validate => :string
+
+  # Set ssl cert file.
+  config :ssl_cert, :validate => :string
+
   ## Set codec.
   default :codec, 'line'
 
@@ -142,7 +157,6 @@ class LogStash::Outputs::WebHdfs < LogStash::Outputs::Base
   end # def register
 
   def receive(event)
-    
     buffer_receive(event)
   end # def receive
 
